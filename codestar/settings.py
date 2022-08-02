df23979092325b37116a01e6bf3d0b4a477f37da
eoghankb <eoghankb@gmail.com>
2022-08-02 21:03:27 +0000

    changed back to codestar

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECTRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['app-devstories.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar2021.herokuapp.com', 'localhost']
 
 
 # Application definition
