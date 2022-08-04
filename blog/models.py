8010cd829540883f61a8c157408804ac8b2e72dc
eoghankb <eoghankb@gmail.com>
2022-08-04 14:40:57 +0000

    Added models for comments

--- blog/models.py
+++ blog/models.py
@@ -14,4 +14,28 @@ class Post(models.model):
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='blog_likes' blank=True)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+class Meta:
+    ordering = ['-created_on']
+
+def _str_(self):
+    return self.title
+
+def number_of_likes(self):
+    return self.likes.count()
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=TRUE)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
