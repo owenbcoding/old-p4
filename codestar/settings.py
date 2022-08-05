15c762d637cf3e6af1da14ae06bc0370c7dee3e9
eoghankb <eoghankb@gmail.com>
2022-08-05 12:16:00 +0000

    Added proper settings for cloudinary

--- codestar/settings.py
+++ codestar/settings.py
@@ -92,6 +92,12 @@ DATABASES = {
     'default': dj_database_url.config(),
 }
 
+CLOUDINARY_STORAGE = { 
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
 # postgres://dspxyvfksevsms:7dc75bfc70f7323bf560272e3147959248ce41b54ac4444aff7f37b93a2b3ce6@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d923q7rf5lb5eq
 # 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
