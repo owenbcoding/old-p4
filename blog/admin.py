ca9bed9ec566fac54b8e01d5c9f8b9475738d6c5
eoghankb <eoghankb@gmail.com>
2022-08-12 22:48:14 +0000

    added search fields and populated fields

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin): 
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
 
