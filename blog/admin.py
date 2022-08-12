cac5e4ed78d5cefbda29f14bbd89d85e97f63368
eoghankb <eoghankb@gmail.com>
2022-08-12 23:08:25 +0000

    readded fields and filter to test if working

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin): 
 
-    list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('content')
-
