aac5014361d1fe284dd81aee8b0e295458294ad9
eoghankb <eoghankb@gmail.com>
2022-08-13 00:53:21 +0000

    Add aprove comments function

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -12,5 +12,6 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on')
     summernote_fields = ('content')
 
-
+    def approve_comments(self, requrest, queryset):
+        queryset.update(approved=True)
 
