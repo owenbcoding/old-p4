3f9933e7aebd83b373caa0098b8d5c458f2fb306
eoghankb <eoghankb@gmail.com>
2022-08-18 18:39:40 +0000

    fixed view for comments

--- blog/views.py
+++ blog/views.py
@@ -12,22 +12,26 @@ class PostList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 6
 
+
 class PostDetail(View):
 
-    def get(self, requrest, slug, *args, **kwargs):
-        queryset = Post.objects.filters(status=1)
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comment = post.comment.filter(approved=True).order_by('created_on')
+        comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
+        # post = get_object_or_404(Post, slug=slug)
 
-        return render(
+        return render( 
             request,
             "post_detail.html", 
-            { 
+            {
              "post": post,
              "comments": comments,
-             "liked": liked
+             "liked": liked,
+            #  "commented": False,
+            #  "comment_form": CommentForm()
             },
         )
