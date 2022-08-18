c0a833e83f53fbf1cad0c2ce5f1566c084e47e51
eoghankb <eoghankb@gmail.com>
2022-08-18 17:56:33 +0000

    add slug url

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 #from .views import index
  
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
 ]
 
