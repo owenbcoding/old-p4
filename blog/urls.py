4040345e0cce80c8c75d0c7974f7c04990e59912
eoghankb <eoghankb@gmail.com>
2022-08-24 06:47:57 +0000

    added url path for like slug

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,10 @@
 from . import views
 from django.urls import path
  
-# importing views from views..py
-#from .views import index
  
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
 
