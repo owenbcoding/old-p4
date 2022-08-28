f61f697c519db1224b8aec7c2884fba2fd07a89e
eoghankb <eoghankb@gmail.com>
2022-08-28 09:25:24 +0000

    Removed Commented out url paths

--- blog/urls.py
+++ blog/urls.py
@@ -10,10 +10,3 @@ urlpatterns = [
     path('newPost/', views.newPost, name='newPost'),
 ]
 
-#path('edit/<int:blog_post_id>/', views.edit_blog, name='edit_blog'),
-#path('delete_blog/<int:blog_post_id>/', views.delete_blog, name='delete_blog'),
-
-#path('post/create/', PostCreateView.as_view(), name='post_create'),
-#path('post/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
-#path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-
