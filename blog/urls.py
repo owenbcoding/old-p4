6179c7e99df0d8a03200999425e881553cac6a21
eoghankb <eoghankb@gmail.com>
2022-08-28 02:58:48 +0000

    Added Path blog post id and path newPost

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,17 @@
 from . import views
 from django.urls import path
+#from .views import PostView, PostCreateView, PostUpdateView, PostDeleteView
  
  
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('<int:blog_post_id>/', views.PostDetail.as_view(), name='blog_detail'),
+    path('newPost/', views.newPost, name='newPost')
 ]
+
+#path('post/create/', PostCreateView.as_view(), name='post_create'),
+#path('post/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
+#path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 
