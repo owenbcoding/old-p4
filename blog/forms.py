6cc7d154dd6c70360d0864294b5de6c4a4581486
eoghankb <eoghankb@gmail.com>
2022-11-10 01:10:26 +0000

    Added blogpostform backend and view

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, Post
 from django.utils.translation import gettext_lazy as _
 from django import forms
 
@@ -9,4 +9,13 @@ class CommentForm(forms.ModelForm):
         fields = ('body',)
         labels = {
             'body': _(''),
+        }
+
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'slug', 'content', 'featured_image', 'excerpt')
+        lables = {
+            'title': _(''), 'slug': _(''), 'content': _(''), 'featured_image': _(''), 'excerpt': _(''),
         }
