29833c85d69b3f739fae23093c48cfbb95ff6ea4
eoghankb <eoghankb@gmail.com>
2022-11-10 01:10:48 +0000

    Removed redunant model

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
 class Post(models.Model):
     """ Model class for post """
     title = models.CharField(max_length=200, unique=True)
@@ -50,16 +51,3 @@ class Comment(models.Model):
     def get_absolute_url(self):
         """ Returns comment with primary key"""
         return reverse('post_detail', kwargs={'pk': self.pk})
-
-
-class newPost():
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
-    featured_image = CloudinaryField('image', default='placeholder')
-    excerpt = models.TextField(blank=True)
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
