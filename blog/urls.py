b3ea08af68d05163807e9f63784c742e2b297957
eoghankb <eoghankb@gmail.com>
2022-11-13 11:45:21 +0000

    Added delete blog url f

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('blog/<int:blog_post_id>/', views.PostDetail.as_view(), name='blog_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('blog/create', views.create_post, name='create_post'),
+    path('blog/delete/<int:blog_post_id>', views.delete_post, name='delete_post'),
 ]
 
